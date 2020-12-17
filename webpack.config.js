8f0a7e01bf1ac454dde21bfd25b01ea19f5a4b78
René Enge <oude.dibbes@gmail.com>
2020-12-17 20:44:35 +0100

    Fresh Node npm install no more Yarn

--- webpack.config.js
+++ webpack.config.js
@@ -1,116 +1,93 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = (env, argv) => ({
-  mode: argv.mode,
-  devtool: argv.mode === 'development' ? 'source-map' : false,
-  entry: ['./src/assets/js/app.js', './src/assets/scss/app.scss'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'assets/js/app.js',
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin(),
-      new OptimizeCSSAssetsPlugin(),
-      new CopyPlugin({
-        patterns: [
-          {
-            from: 'src/*.html',
-            to: '[name].[ext]',
-          },
-          {
-            from: 'src/assets/img/**/*',
-            to: 'assets/img/[name].[ext]',
-          },
-        ],
-      }),
+	mode: argv.mode,
+	devtool: 'source-map',
+	entry: ['./src/assets/js/app.js', './src/assets/scss/app.scss'],
+	output: {
+		publicPath: 'dist',
+		filename: 'assets/js/app.js',
+		chunkFilename: 'js/[name].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	optimization: {
+		usedExports: true,
+		minimizer: [
+			new UglifyJsPlugin({
+				cache: true,
+				parallel: true,
+				sourceMap: argv.mode === 'development',
+			}),
+			new OptimizeCSSAssetsPlugin({}),
+		],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.(scss)$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							sourceMap: argv.mode === 'development',
+						},
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							url: false,
+							sourceMap: argv.mode === 'development',
+						},
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins() {
+								return [require('autoprefixer')];
+							},
+							sourceMap: argv.mode === 'development',
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: argv.mode === 'development',
+							implementation: require('sass'),
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+			'windows.jQuery': 'jquery',
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].css',
+			chunkFilename: 'css/[id].css',
+		}),
+		new CleanWebpackPlugin(),
     ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+    devServer: {
+        contentBase: [path.join(__dirname, '/src')],
+        watchContentBase: true,
+        compress: true,
+        port: 9000,
+        open: true,
       },
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              url: false,
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [['autoprefixer']],
-              },
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              implementation: require('sass'),
-            },
-          },
-        ],
-      },
-      {
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-        },
-        test: /\.(jpe?g|png|gif|svg)$/i,
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'assets/css/app.css',
-    }),
-    new ImageMinimizerPlugin({
-      minimizerOptions: {
-
-        // Lossless optimization with custom option
-        // Feel free to experiment with options for better result for you
-        plugins: [
-          ['gifsicle', { interlaced: true }],
-          ['jpegtran', { progressive: true }],
-          ['optipng', { optimizationLevel: 5 }],
-          [
-            'svgo',
-            {
-              plugins: [
-                {
-                  removeViewBox: false,
-                },
-              ],
-            },
-          ],
-        ],
-      },
-    }),
-  ],
-  devServer: {
-    contentBase: [path.join(__dirname, '/src')],
-    watchContentBase: true,
-    compress: true,
-    port: 9000,
-    open: true,
-  },
 });
